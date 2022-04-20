a34e62c0030a0ae295bc55dee532ca7828ee9389
Michael Polivka <michael.polivka1@gmail.com>
2022-04-20 19:31:23 +0200

    Initial Commit

---
+++ Projekt2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Projekt2
+//
+//  Created by Michael Polívka on 20.04.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
